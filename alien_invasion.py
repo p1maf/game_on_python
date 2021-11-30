deb46e640ab1b8ce735084db38b8992307d3cf12
ilya_ermakov <ilya-ermakov.2003@yandex.ru>
2021-12-01 01:06:06 +0300

    Изменение скорости корабля, рефакторинг кода(создание отдельных функций)

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,9 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         # Назнаение цвета фона.
@@ -27,19 +29,26 @@ class AlienInvasion:
     def _check_events(self):
         """Обрабатывает нажатия клавиш и события мыши."""
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.K_q:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
 
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
         self.screen.fill(self.settings.bg_color)
--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
 
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загружает изображение корабля и получает прямоугольник
@@ -14,16 +15,22 @@ class Ship:
         # Каждый новый корабль появляется у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+
         # Флаг перемещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Обновляет позицию корабля с учетом флага."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # Обновляется атрибут x, не rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        # Обновление атрибута rect на основании self.x
+        self.rect.x = self.x
     def bltime(self):
         """"Ризует корабль в текущей позиции"""
         self.screen.blit(self.image, self.rect)
