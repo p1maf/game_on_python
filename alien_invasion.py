fd49a3845e2524fc1e0266c89276b09c9c547ffa
ilya_ermakov <ilya-ermakov.2003@yandex.ru>
2021-12-01 00:21:00 +0300

    Добавление движение корабля влево и вправо

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 from settings import Settings
+from ship import Ship
 import pygame
 
 
@@ -12,22 +13,38 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
+        self.ship = Ship(self)
         # Назнаение цвета фона.
         self.bg_color = (230, 230, 230)
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            # При каждом проходе цикла перерисоывавается экран.
-            self.screen.fill(self.settings.bg_color)
 
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.bltime()
+        pygame.display.flip()
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
     ai = AlienInvasion()
--- ship.py
+++ ship.py
@@ -9,10 +9,21 @@ class Ship:
 
         # Загружает изображение корабля и получает прямоугольник
         self.image = pygame.image.load('icon//1.svg')
+        self.image = pygame.transform.scale(self.image, (60, 60))
         self.rect = self.image.get_rect()
         # Каждый новый корабль появляется у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Обновляет позицию корабля с учетом флага."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
     def bltime(self):
         """"Ризует корабль в текущей позиции"""
         self.screen.blit(self.image, self.rect)
