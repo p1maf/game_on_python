8b29630a2a7b9af67da047064bad92a8cd6d6836
ilya_ermakov <ilya-ermakov.2003@yandex.ru>
2021-12-01 01:24:46 +0300

    Добавление пуль.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,9 @@
 import sys
+import pygame
+
 from settings import Settings
 from ship import Ship
-import pygame
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -16,6 +18,7 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         # Назнаение цвета фона.
         self.bg_color = (230, 230, 230)
     def run_game(self):
@@ -23,6 +26,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
 
@@ -43,17 +47,37 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Создание нового снаряда и включение его в группу bullets"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
         self.screen.fill(self.settings.bg_color)
         self.ship.bltime()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
+    def _update_bullets(self):
+        """Обновляем позиции снарядов и уничтожаем старые снаряды"""
+        # Обновление позиций снарядов
+        self.bullets.update()
+
+        # Удаление снарядов вышедших за край экрана
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
     ai = AlienInvasion()
